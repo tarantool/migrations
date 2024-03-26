481a6b3aa595228549edb04ce3ca226b95c8f209
psergee <p_sergee@mail.ru>
2024-03-26 12:29:14 +0300

    ci: make tests cleanup consistent
    If the cleanup process is done for some tests after they have run,
    and for others - before, this can lead to a situation where
    cleanup is not called at all. To avoid this, make all tests use
    the same cleanup scheme: cleanup after each test.

--- test/integration/console_run_test.lua
+++ test/integration/console_run_test.lua
@@ -45,6 +45,7 @@ g.cluster = cartridge_helpers.Cluster:new({
 
 g.before_all(function() g.cluster:start() end)
 g.after_all(function() g.cluster:stop() end)
+g.after_each(function() utils.cleanup(g) end)
 
 local cases = {
     with_config_loader = function()
@@ -77,7 +78,6 @@ local cases = {
 
 for k, configure_func in pairs(cases) do
     g['test_run_from_console_' .. k] = function()
-        utils.cleanup(g)
         configure_func()
 
         for _, server in pairs(g.cluster.servers) do
