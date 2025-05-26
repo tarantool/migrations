e4b4d00d86fde13cc5be9329bcdd9fa900e5a91d
Igor Zolotarev <63460867+yngvar-antonsson@users.noreply.github.com>
2025-05-26 11:22:48 +0300

    migrations: fix race in validate config
    (cherry picked from commit 83468c9c7bb4413fd0b7259d1e0eab2ae8faf9ca)

--- deps.sh
+++ deps.sh
@@ -4,7 +4,7 @@
 set -e
 
 # Test dependencies:
-tarantoolctl rocks install luatest 0.5.7
+tarantoolctl rocks install luatest 1.0.1
 tarantoolctl rocks install luacov 0.13.0
 tarantoolctl rocks install luacheck 0.26.0
 tarantoolctl rocks make migrations-scm-1.rockspec
---
+++ test/unit/apply_config_test.lua
@@ -0,0 +1,25 @@
+local t = require("luatest")
+local g = t.group()
+
+g.after_each(function()
+    if box.space._migrations ~= nil then
+        box.space._migrations:drop()
+    end
+end)
+
+g.test_apply_config_order = function()
+    box.ctl.on_schema_init(function()
+        box.space._space:on_replace(function(_, sp)
+            if sp.name == '_migrations' then
+
+                require('fiber').sleep(3)
+            end
+        end)
+    end)
+    local migrator = require('migrator')
+    package.setsearchroot("test/unit")
+    t.assert(pcall(migrator.validate_config,{}, {}))
+    t.assert(pcall(migrator.init))
+    t.assert(pcall(migrator.validate_config,{}, {}))
+    t.assert(pcall(migrator.apply_config,{is_master = true}, {}))
+end
